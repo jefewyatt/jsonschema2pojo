109841a8dab2cabb1c49ceea5c266cfbc792ad94
jclyne1 <jeff_clyne@intuit.com>
2015-07-14 09:58:44 -0700

    adding schema rule

--- jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/rules/SchemaRule.java
+++ jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/rules/SchemaRule.java
@@ -16,9 +16,20 @@
 
 package org.jsonschema2pojo.rules;
 
+import static org.apache.commons.lang3.StringUtils.capitalize;
+
+import java.util.LinkedList;
+
 import com.fasterxml.jackson.databind.JsonNode;
+
 import org.jsonschema2pojo.Schema;
+
+import com.sun.codemodel.ClassType;
+import com.sun.codemodel.JClassAlreadyExistsException;
 import com.sun.codemodel.JClassContainer;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JPackage;
 import com.sun.codemodel.JType;
 
 /**
@@ -38,23 +49,23 @@ public class SchemaRule implements Rule<JClassContainer, JType> {
     /**
      * Applies this schema rule to take the required code generation steps.
      * <p>
-     * At the root of a schema document this rule should be applied (schema
-     * documents contain a schema), but also in many places within the document.
-     * Each property of type "object" is itself defined by a schema, the items
-     * attribute of an array is a schema, the additionalProperties attribute of
-     * a schema is also a schema.
+     * At the root of a schema document this rule should be applied (schema documents contain a schema), but also in
+     * many places within the document. Each property of type "object" is itself defined by a schema, the items
+     * attribute of an array is a schema, the additionalProperties attribute of a schema is also a schema.
      * <p>
-     * Where the schema value is a $ref, the ref URI is assumed to be applicable
-     * as a URL (from which content will be read). Where the ref URI has been
-     * encountered before, the root Java type created by that schema will be
-     * re-used (generation steps won't be repeated).
+     * Where the schema value is a $ref, the ref URI is assumed to be applicable as a URL (from which content will be
+     * read). Where the ref URI has been encountered before, the root Java type created by that schema will be re-used
+     * (generation steps won't be repeated).
      * 
      * @param schema
      *            the schema within which this schema rule is being applied
      */
     @Override
     public JType apply(String nodeName, JsonNode schemaNode, JClassContainer generatableType, Schema schema) {
-
+        System.err.println("NAME " + nodeName);
+        if (nodeName.equals("view")) {
+            System.out.println("break");
+        }
         if (schemaNode.has("$ref")) {
             schema = ruleFactory.getSchemaStore().create(schema, schemaNode.get("$ref").asText());
             schemaNode = schema.getContent();
@@ -66,6 +77,37 @@ public class SchemaRule implements Rule<JClassContainer, JType> {
             return apply(nodeName, schemaNode, generatableType, schema);
         }
 
+        // Trying the oneOf bullshit
+        if (schemaNode.has("oneOf") || schemaNode.has("anyOf")) {
+            // create the empty polymorphic container
+
+            System.err.println("Node name: " + nodeName);
+            JDefinedClass superType;
+            try {
+                superType = generatableType.owner()._class(JMod.PUBLIC, getClassName(nodeName, generatableType.getPackage()), ClassType.CLASS);
+            } catch (JClassAlreadyExistsException e) {
+                superType = e.getExistingClass();
+            }
+            System.err.println("Super type: " + superType.name());
+
+            // Now let's iterate through the schemas and get their types:
+            for (JsonNode childSchema : schemaNode.get("oneOf")) {
+                if (childSchema.has("$ref")) {
+                    int index = childSchema.get("$ref").asText().lastIndexOf("/");
+                    nodeName.add(childSchema.get("$ref").asText().substring(index));
+                    System.out.println("=========" + nodeName);
+                }
+                JType childType = apply(nodeName, childSchema, generatableType, schema);
+                // let's see if this cast works =/
+                if (childType instanceof JDefinedClass) {
+                    JDefinedClass childClass = (JDefinedClass) childType;
+
+                    childClass._extends(superType);
+                }
+            }
+            return superType;
+        }
+
         JType javaType;
         if (schemaNode.has("enum")) {
             javaType = ruleFactory.getEnumRule().apply(nodeName, schemaNode, generatableType, schema);
@@ -73,7 +115,13 @@ public class SchemaRule implements Rule<JClassContainer, JType> {
             javaType = ruleFactory.getTypeRule().apply(nodeName, schemaNode, generatableType.getPackage(), schema);
         }
         schema.setJavaTypeIfEmpty(javaType);
-
+        System.err.println("construct type: " + javaType.name());
         return javaType;
+    }
+
+    private String getClassName(String nodeName, JPackage _package) {
+        String className = ruleFactory.getNameHelper().replaceIllegalCharacters(capitalize(nodeName));
+        String normalizedName = ruleFactory.getNameHelper().normalizeName(className);
+        return normalizedName;
     }
 }
